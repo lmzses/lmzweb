ec043dee0ad301dc5b46acf02ffdf97e44bf4a29
Lmzses <ml776566@gmail.com>
2025-01-04 11:06:36 -0500

    Integrate Vercel adapter and update dependencies; add type annotations for blog post sorting and collection retrieval in Astro pages.

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,10 @@ import tailwind from '@astrojs/tailwind';
 
 import mdx from '@astrojs/mdx';
 
+import vercel from '@astrojs/vercel';
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), mdx()]
+  integrations: [tailwind(), mdx()],
+  adapter: vercel()
 });
